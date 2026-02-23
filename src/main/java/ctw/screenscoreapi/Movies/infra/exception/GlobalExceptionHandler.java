29a3db031b1d4fea30613d8058aae619a770bde0
Adryel <adryxd003@gmail.com>
2026-02-23 19:39:22 -0300

    feat: adiciona handlers de exceções genéricas e específicas em GlobalExceptionHandler

--- src/main/java/ctw/screenscoreapi/Movies/infra/exception/GlobalExceptionHandler.java
+++ src/main/java/ctw/screenscoreapi/Movies/infra/exception/GlobalExceptionHandler.java
@@ -1,15 +1,19 @@
 package ctw.screenscoreapi.Movies.infra.exception;
 
 import ctw.screenscoreapi.Movies.application.exceptions.MovieApplicationException;
+import feign.FeignException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -18,12 +22,12 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @Value("${BASE_URL}")
+    @Value("${BASE_URL}" + "/errors")
     private String BASE_URL;
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ProblemDetail> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) throws URISyntaxException {
-        URI type = new URI(BASE_URL + "/errors/invalid-argument");
+        URI type = new URI(BASE_URL + "/invalid-argument");
         URI instance = new URI(request.getRequestURI());
         String title = "A requisição contém campos inválidos";
         HttpStatus status = HttpStatus.BAD_REQUEST;
@@ -48,7 +52,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MovieApplicationException.class)
     public ResponseEntity<ProblemDetail> handleMovieApplicationException(MovieApplicationException e, HttpServletRequest request) throws URISyntaxException {
-        URI type = new URI(BASE_URL + "/errors/business-rule");
+        URI type = new URI(BASE_URL + "/business-rule");
         URI instance = new URI(request.getRequestURI());
         String title = "Violação de regra de negócio";
         String detail = e.getMessage();
@@ -59,6 +63,95 @@ public class GlobalExceptionHandler {
         problemDetail.setInstance(instance);
         problemDetail.setDetail(detail);
         problemDetail.setTitle(title);
+
+        return ResponseEntity
+                .status(status)
+                .body(problemDetail);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ProblemDetail> handleNoResourceFoundException(HttpServletRequest httpRequest) throws URISyntaxException {
+        URI type = new URI(BASE_URL + "/no-resource-found");
+        URI instace = new URI(httpRequest.getRequestURI());
+        String title = "Recurso não encontrado";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setType(type);
+        problemDetail.setTitle(title);
+        problemDetail.setInstance(instace);
+
+        return ResponseEntity
+                .status(status)
+                .body(problemDetail);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ProblemDetail> handleMethodNotAllowedException(HttpRequestMethodNotSupportedException e, HttpServletRequest httpRequest) throws URISyntaxException {
+        URI type = new URI(BASE_URL + "/method-not-allowed");
+        URI instance = new URI(httpRequest.getRequestURI());
+        String title = "O método " + httpRequest.getMethod() + " não é permitido";
+        String detail = "Ações permitidas: " + String.join(", ", e.getSupportedMethods());
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setType(type);
+        problemDetail.setInstance(instance);
+        problemDetail.setTitle(title);
+        problemDetail.setDetail(detail);
+
+        return ResponseEntity
+                .status(status)
+                .body(problemDetail);
+    }
+
+    @ExceptionHandler(FeignException.class)
+    public ResponseEntity<ProblemDetail> handleFeignException(FeignException e, HttpServletRequest httpRequest) throws URISyntaxException {
+        URI type = new URI(BASE_URL + "/external-server");
+        URI instance = new URI(httpRequest.getRequestURI());
+        String title = "Erro ao se comunicar com sistema externo";
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setType(type);
+        problemDetail.setInstance(instance);
+        problemDetail.setTitle(title);
+
+        return ResponseEntity
+                .status(status)
+                .body(problemDetail);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ProblemDetail> handleMessageNotReadableException(HttpServletRequest httpRequest) throws URISyntaxException {
+        URI type = new URI(BASE_URL + "/invalid-body");
+        URI instace = new URI(httpRequest.getRequestURI());
+        String title = "O body da requisição está ausente ou mal formatado";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setType(type);
+        problemDetail.setInstance(instace);
+        problemDetail.setTitle(title);
+
+        return ResponseEntity
+                .status(status)
+                .body(problemDetail);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ProblemDetail> handleException(HttpServletRequest httpRequest) throws URISyntaxException {
+        URI type = new URI(BASE_URL + "/generic-exception");
+        URI instance = new URI(httpRequest.getRequestURI());
+        String title = "Exceção genérica";
+        String detail = "O servidor encontrou um erro inesperado";
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setType(type);
+        problemDetail.setInstance(instance);
+        problemDetail.setTitle(title);
+        problemDetail.setDetail(detail);
 
         return ResponseEntity
                 .status(status)

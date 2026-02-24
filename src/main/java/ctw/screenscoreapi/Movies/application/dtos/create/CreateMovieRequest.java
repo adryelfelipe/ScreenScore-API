f5e5adbfff0d29e00376a1dfa5bfb0015e7aa8d9
Adryel Felipe <adryxd003@gmail.com>
2026-02-24 14:14:49 -0300

    feat: documenta funcionalidade de criar filmes em MovieController

--- src/main/java/ctw/screenscoreapi/Movies/application/dtos/create/CreateMovieRequest.java
+++ src/main/java/ctw/screenscoreapi/Movies/application/dtos/create/CreateMovieRequest.java
@@ -11,28 +11,36 @@ import java.util.List;
 
 public record CreateMovieRequest(@NotBlank(message = "O título do filme é obrigatório")
                                  @Size(min = 2, max = 255, message = "O título do filme deve possuir entre 2 e 255 caracteres")
+                                 @Schema(example = "ScreenScore, the best movie")
                                  String title,
 
                                  @Size(min = 2, max = 4, message = "A língua original deve possui entre 2 e 4 caracteres")
+                                 @Schema(example = "user@example.com")
                                  String originalLanguage,
 
                                  @Size(max = 255, message = "O título do filme deve possuir no máximo 255 caracteres")
+                                 @Schema(example = "pt")
                                  String originalTitle,
 
                                  @NotNull(message = "A classificação etária do filme é obrigatória")
+                                 @Schema(example = "false")
                                  Boolean adult,
 
                                  @NotBlank(message = "A data de lançamento do filme é obrigatório")
                                  @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$",
                                          message = "A data deve estar no formato yyyy-MM-dd")
+                                 @Schema(example = "2025-02-24")
                                  String releaseDate,
 
+                                 @Schema(description = "Referência da imagem obtida ao se comunicar com ThemovieDB", example = "/referencia-da-imagem-do-themoviedb.jpg")
                                  String posterImage,
 
                                  @Size(min = 3, message = "A visão geral do filme deve possuir no mínimo 5 caracteres")
                                  @NotBlank(message = "A visão geral do filme é obrigatória")
+                                 @Schema(example = "ScreenScore é um filme de ficação científica")
                                  String overview,
 
                                  @NotNull(message = "O gênero do filme é obrigatório")
+                                 @Schema(example = "FICCAO_CIENTIFICA")
                                  List<Genre> genres
 ) {}

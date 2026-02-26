8d0fa97492755885ebe295a9af215338ea650296
Adryel <adryxd003@gmail.com>
2026-02-25 21:34:15 -0300

    docs: adiciona exemplo de campo title para Schema de GetExternalMovieRequest

--- src/main/java/ctw/screenscoreapi/Movies/application/dtos/get/GetExternalMovieRequest.java
+++ src/main/java/ctw/screenscoreapi/Movies/application/dtos/get/GetExternalMovieRequest.java
@@ -1,6 +1,10 @@
 package ctw.screenscoreapi.Movies.application.dtos.get;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
-public record GetExternalMovieRequest(@NotBlank(message = "O título do filme é obrigatório") String title) {
-}
+public record GetExternalMovieRequest(
+        @NotBlank(message = "O título do filme é obrigatório")
+        @Schema(example = "Minions")
+        String title
+) {}

9ae4e22e6828635541ab72d7850b63dc60ca02c8
Adryel Felipe <adryxd003@gmail.com>
2026-02-26 14:04:16 -0300

    docs: adiciona exemplo padrão em GetExternalMovieResponse

--- src/main/java/ctw/screenscoreapi/Movies/application/controller/MovieController.java
+++ src/main/java/ctw/screenscoreapi/Movies/application/controller/MovieController.java
@@ -74,46 +74,7 @@ public class MovieController {
             @ApiResponse(
                     responseCode = "200",   
                     description = "Lista de filmes retornada com sucesso",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = GetExternalMovieResponse.class, example =
-                            """
-                            {
-                                    "movies": [
-                                            {
-                                                "title": "Minions",
-                                                "originalLanguage": "en",
-                                                "originalTitle": "Minions",
-                                                "adult": false,
-                                                "releaseDate": "2015-06-17",
-                                                "posterImage": "/caq9Xi6b1sZNREfzFBO2tRIBzWn.jpg",
-                                                "overview": "Seres amarelos milenares, os minions têm uma missão: servir os maiores vilões.", 
-                                                "genres": [
-                                                    "FAMILIA",
-                                                    "ANIMACAO",
-                                                    "AVENTURA",
-                                                    "COMEDIA"
-                                                ]
-                                            },
-                                            {
-                                                "title": "Minions 2: A Origem de Gru",
-                                                "originalLanguage": "en",
-                                                "originalTitle": "Minions: The Rise of Gru",
-                                                "adult": false,
-                                                "releaseDate": "2022-06-29",
-                                                "posterImage": "/iTP3mMw0AoqmScYzDoMmYeKxYe.jpg",
-                                                "overview": "A continuação das aventuras dos Minions, sempre em busca de um líder tirânico. Dessa vez, eles ajudam um Gru ainda criança, descobrindo como ser vilão.",
-                                                "genres": [
-                                                    "FAMILIA",
-                                                    "COMEDIA",
-                                                    "AVENTURA",
-                                                    "ANIMACAO",
-                                                    "FICCAO_CIENTIFICA",
-                                                    "FANTASIA"
-                                                ]
-                                           }
-                                     ]       
-                            }        
-                            """
-                    ))
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = GetExternalMovieResponse.class))
             ),
 
             @ApiResponse(

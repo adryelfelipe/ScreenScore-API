da631b756e75a2fe0ad7eb5e2b74a7dc14059856
Adryel <adryxd003@gmail.com>
2026-03-23 17:57:02 -0300

    docs: adiciona documentação da funcionalidade de criar usuários

--- src/main/java/ctw/screenscoreapi/Users/application/controller/UserController.java
+++ src/main/java/ctw/screenscoreapi/Users/application/controller/UserController.java
@@ -3,6 +3,10 @@ package ctw.screenscoreapi.Users.application.controller;
 import ctw.screenscoreapi.Users.application.dtos.create.CreateUserRequest;
 import ctw.screenscoreapi.Users.application.dtos.get.GetUserResponse;
 import ctw.screenscoreapi.Users.application.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +15,7 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/usuarios")
+@Tag(name = "Usuarios", description = "Endpoints para gerenciamento de usuários")
 public class UserController {
     private UserService userService;
 
@@ -18,6 +23,32 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(
+            summary = "Cria usuário no sistema",
+            description = "Cria um novo usuário no sistema caso não exista nenhum outro com o mesmo email"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Usuário criado com sucesso!"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    ref = "#/components/responses/400"
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    ref = "#/components/responses/401"
+            ),
+            @ApiResponse(
+                    responseCode = "409",
+                    ref = "#/components/responses/409"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    ref = "#/components/responses/500"
+            )
+    })
     @PostMapping
     public ResponseEntity<Void> create(@Valid @RequestBody CreateUserRequest request) {
         long id = userService.create(request);
--- src/main/java/ctw/screenscoreapi/Users/application/dtos/create/CreateUserRequest.java
+++ src/main/java/ctw/screenscoreapi/Users/application/dtos/create/CreateUserRequest.java
@@ -1,14 +1,17 @@
 package ctw.screenscoreapi.Users.application.dtos.create;
 
 import ctw.screenscoreapi.Users.domain.enums.Role;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 
 public record CreateUserRequest(
         @NotBlank(message = "O nome é obrigatório")
+        @Schema(example = "Robson Cavalo")
         String name,
 
         @Email(message = "O formato do email deve estar válido")
         @NotBlank(message = "O email é obrigatório")
+        @Schema(example = "robsoncavalo@gmail.com")
         String email,
 
         @Size(min = 8, max = 100, message = "Senha deve ter entre 8 e 100 caracteres")
@@ -17,8 +20,10 @@ public record CreateUserRequest(
                 message = "Senha deve conter pelo menos 1 letra maiúscula, 1 minúscula, 1 número e 1 símbolo"
         )
         @NotBlank(message = "A senha é obrigatória")
+        @Schema(example = "Abcdefg@123")
         String password,
 
         @NotNull(message = "O tipo de usuário é obrigatório")
+        @Schema(example = "CLIENTE")
         Role role)
 {}

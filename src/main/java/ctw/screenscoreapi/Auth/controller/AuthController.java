a4729c5705f420cf42b0bec15695b3ef97bed308
Adryel <adryxd003@gmail.com>
2026-03-23 16:25:38 -0300

    docs: adiciona documentação para funcionalidade de registro no sistema

--- src/main/java/ctw/screenscoreapi/Auth/controller/AuthController.java
+++ src/main/java/ctw/screenscoreapi/Auth/controller/AuthController.java
@@ -1,8 +1,12 @@
-package ctw.screenscoreapi.Auth.application;
+package ctw.screenscoreapi.Auth.controller;
 
 import ctw.screenscoreapi.Auth.dtos.LoginRequest;
 import ctw.screenscoreapi.Auth.dtos.RegisterRequest;
 import ctw.screenscoreapi.Auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +18,7 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "Autenticação", description = "Endpoints para gerenciamento de autenticação")
 public class AuthController {
     private AuthService authService;
 
@@ -21,6 +26,28 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @Operation(
+            summary = "Registra um novo usuário no sistema",
+            description = "Registra um novo usuário no sistema caso esteja válido e não exista nenhum outro com o mesmo email"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Usuário registrado com sucesso"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    ref = "#/components/responses/400"
+            ),
+            @ApiResponse(
+                    responseCode =  "409",
+                    ref = "#/components/responses/409"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    ref = "#/components/responses/500"
+            )
+    })
     @PostMapping("/register")
     public ResponseEntity<Void> register(@Valid @RequestBody RegisterRequest request) {
         long id = authService.register(request);
--- src/main/java/ctw/screenscoreapi/Auth/dtos/RegisterRequest.java
+++ src/main/java/ctw/screenscoreapi/Auth/dtos/RegisterRequest.java
@@ -1,5 +1,6 @@
 package ctw.screenscoreapi.Auth.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -7,10 +8,12 @@ import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
         @NotBlank(message = "O nome é obrigatório")
+        @Schema(description = "Nome do usuário", example = "Adryel Sapelli")
         String name,
 
         @Email(message = "O formato do email deve estar válido")
         @NotBlank(message = "O email é obrigatório")
+        @Schema(description = "Email do usuário", example = "adryelsapelli@gmail.com")
         String email,
 
         @Size(min = 8, max = 100, message = "Senha deve ter entre 8 e 100 caracteres")
@@ -19,5 +22,6 @@ public record RegisterRequest(
                 message = "Senha deve conter pelo menos 1 letra maiúscula, 1 minúscula, 1 número e 1 símbolo"
         )
         @NotBlank(message = "A senha é obrigatória")
+        @Schema(description = "Senha do usuário", example = "Abcdefg123@")
         String password
 ) {}

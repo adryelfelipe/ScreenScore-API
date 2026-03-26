6411314b699df6c3e996cdf6ece5dfe7c6180301
Adryel <adryxd003@gmail.com>
2026-03-25 21:45:35 -0300

    docs: adiciona documentação em DTO de Login

--- src/main/java/ctw/screenscoreapi/Auth/dtos/LoginRequest.java
+++ src/main/java/ctw/screenscoreapi/Auth/dtos/LoginRequest.java
@@ -1,5 +1,6 @@
 package ctw.screenscoreapi.Auth.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -8,6 +9,7 @@ import jakarta.validation.constraints.Size;
 public record LoginRequest(
         @Email(message = "O formato do email deve estar válido")
         @NotBlank(message = "O email é obrigatório")
+        @Schema(example = "adryelgamer@gmail.com")
         String email,
 
         @Size(min = 8, max = 100, message = "Senha deve ter entre 8 e 100 caracteres")
@@ -16,5 +18,6 @@ public record LoginRequest(
                 message = "Senha deve conter pelo menos 1 letra maiúscula, 1 minúscula, 1 número e 1 símbolo"
         )
         @NotBlank(message = "A senha é obrigatória")
+        @Schema(example = "123456Aa@")
         String password
 ) {}

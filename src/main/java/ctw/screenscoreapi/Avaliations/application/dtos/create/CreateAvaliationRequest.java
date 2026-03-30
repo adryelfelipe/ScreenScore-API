a10386cad875a355bc20078ae1c01ee4e54c088f
Adryel <adryxd003@gmail.com>
2026-03-30 20:21:25 -0300

    docs: adiciona a documentação de CreateAvaliationRequest DTO

--- src/main/java/ctw/screenscoreapi/Avaliations/application/dtos/create/CreateAvaliationRequest.java
+++ src/main/java/ctw/screenscoreapi/Avaliations/application/dtos/create/CreateAvaliationRequest.java
@@ -1,18 +1,22 @@
 package ctw.screenscoreapi.Avaliations.application.dtos.create;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 
 public record CreateAvaliationRequest(
         @NotNull(message = "O número identificador do filme avaliado é obrigatório")
         @Positive(message = "O número identificador do filme avaliado deve ser positivo")
+        @Schema(example = "2")
         Long movieId,
 
         @NotNull(message = "A nota do filme avaliado é obrigatória")
         @DecimalMax(value = "10.0", inclusive = true, message = "A nota do filme avaliado deve ser no máximo 10.0")
         @DecimalMin(value = "0.0", inclusive = true, message = "A nota do filme avaliado deve ser no mínimo 0.0")
+        @Schema(example = "10.0")
         Double score,
 
         @Size(min = 2, message = "O comentário da avaliação deve ter no mínimo 2 caracteres")
         @NotBlank(message = "O comentário da avaliação é obrigatório")
+        @Schema(example = "Filme muito bom! Recomendo")
         String comment
 ) {}

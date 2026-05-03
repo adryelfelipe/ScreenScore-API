821c5eb7033006a54780962684bc055a3cab4fa5
Adryel Felipe <adryxd003@gmail.com>
2026-05-03 15:21:42 -0300

    test: add unit test for login with invalid password use case

--- src/test/java/ctw/screenscoreapi/auth/AuthServiceTest.java
+++ src/test/java/ctw/screenscoreapi/auth/AuthServiceTest.java
@@ -2,9 +2,11 @@ package ctw.screenscoreapi.auth;
 
 import ctw.screenscoreapi.Module.Auth.dtos.LoginRequest;
 import ctw.screenscoreapi.Module.Auth.dtos.RegisterRequest;
+import ctw.screenscoreapi.Module.Auth.exception.InvalidCredentialsException;
 import ctw.screenscoreapi.Module.Auth.mapper.AuthMapper;
 import ctw.screenscoreapi.Module.Auth.service.AuthService;
 import ctw.screenscoreapi.Module.Users.application.dtos.create.CreateUserRequest;
+import ctw.screenscoreapi.Module.Users.application.exception.UserNotFoundByEmailException;
 import ctw.screenscoreapi.Module.Users.application.service.UserService;
 import ctw.screenscoreapi.Module.Users.domain.entity.UserEntity;
 import ctw.screenscoreapi.Module.Users.domain.enums.Role;
@@ -16,9 +18,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
@@ -67,5 +71,20 @@ public class AuthServiceTest {
         verify(userService).getFullUserByEmail(loginRequest.email());
         verify(userSession).setUserId(userEntity.getId());
         verify(userSession).setRole(userEntity.getRole());
+    }
+
+    @Test
+    @DisplayName("Should throw InvalidCredentialsException when password is invalid")
+    public void shouldThrowInvalidCredentialsExceptionWhenPasswordIsInvalid() {
+        // Arrange
+        LoginRequest loginRequest = new LoginRequest("pedro@gmail.com", "123456@Aa");
+        UserEntity userEntity = new UserEntity(1L, "otherPassword", "Pedro", loginRequest.email(), Role.CLIENT);
+        when(userService.getFullUserByEmail(loginRequest.email())).thenReturn(userEntity);
+
+        // Assert + Act
+        assertThrows(InvalidCredentialsException.class, () -> authService.login(loginRequest));
+        verify(userService).getFullUserByEmail(loginRequest.email());
+        verify(userSession, never()).setUserId(any());
+        verify(userSession, never()).setRole(any());
     }
 }

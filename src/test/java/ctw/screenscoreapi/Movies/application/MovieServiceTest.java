d32fefa7d6ee61a85d25b7d2130c1b92f0083f1f
Adryel Felipe <adryxd003@gmail.com>
2026-05-12 11:58:46 -0300

    test: add unit test for creating movie with invalid title use case

--- src/test/java/ctw/screenscoreapi/Movies/application/MovieServiceTest.java
+++ src/test/java/ctw/screenscoreapi/Movies/application/MovieServiceTest.java
@@ -1,6 +1,7 @@
 package ctw.screenscoreapi.Movies.application;
 
 import ctw.screenscoreapi.Module.Movies.application.dtos.create.CreateMovieRequest;
+import ctw.screenscoreapi.Module.Movies.application.exceptions.MovieTitleAlreadyUsedException;
 import ctw.screenscoreapi.Module.Movies.application.mapper.MovieMapper;
 import ctw.screenscoreapi.Module.Movies.application.service.MovieService;
 import ctw.screenscoreapi.Module.Movies.domain.entity.MovieEntity;
@@ -24,6 +25,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -82,5 +84,29 @@ public class MovieServiceTest {
         verify(movieMapper).toEntity(request, posterKey);
         verify(movieRepository).create(movie);
         assertThat(movieId).isEqualTo(movie.getId());
+    }
+
+    @Test
+    @DisplayName("Should throw MovieTitleAlreadyUsedException when the movie title is already used")
+    public void shouldThrowMovieTitleAlreadyUsedExceptionWhenTheMovieTitleIsAlreadyUsed() throws IOException {
+        // Arrange
+        String posterKey = "123";
+        String title =  "ScreenScore, o melhor filme de todos!";
+        String releaseDate = "2025-02-24";
+        String originalLanguage = "en";
+        String originalTitle = "ScreenScore, the best movie!";
+        Boolean adult  = false;
+        String overview = "ScreenScore é um filme de ficação científica";
+        List<Genre> genres = List.of(Genre.FICCAO_CIENTIFICA, Genre.ACAO);
+        MovieEntity movie = new MovieEntity(1L, posterKey, releaseDate, adult, originalTitle, originalLanguage, title, overview, genres, null, null);
+        CreateMovieRequest request = new CreateMovieRequest(title, originalLanguage, originalTitle, adult, releaseDate, overview, genres);
+        when(movieRepository.findByExactTitle(request.title())).thenReturn(Optional.of(movie));
+
+        // Asserts + Act
+        assertThrows(MovieTitleAlreadyUsedException.class, () -> movieService.create(request, file));
+        verify(movieRepository, never()).create(any());
+        verify(movieRepository).findByExactTitle(request.title());
+        verifyNoInteractions(movieMapper);
+        verifyNoInteractions(s3Service);
     }
 }
